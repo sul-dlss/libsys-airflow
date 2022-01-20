85083ba933988bb2cea4c3ae1c5b16652f6dfdfd
Jeremy Nelson <jermnelson@gmail.com>
2022-01-20 16:15:38 -0700

    Refactor to use python function instead of bash to filter for MARC holdings

--- dags/bib_records.py
+++ dags/bib_records.py
@@ -1,8 +1,11 @@
 """Imports exported MARC records from Symphony into FOLIO"""
 
 from datetime import datetime, timedelta
-import pathlib
 import json
+import logging
+import pathlib
+
+import shutil
 from textwrap import dedent
 from typing_extensions import TypeAlias
 
@@ -16,26 +19,29 @@ from airflow.sensors.filesystem import FileSensor
 
 from folio_post import post_folio_instance_records
 
-def read_marc(*args, **kwargs) -> list:
-    """Stub function for reading MARC21 records"""
-    print("in read_marc")
-    return []
+logger = logging.getLogger(__name__)
 
 
-def convert_to_folio(*args, **kwargs) -> list:
-    """Stub function for converting list of MARC21 to FOLIO json records"""
-    return []
-
-
-def load_records(*args, **kwargs) -> bool:
-    """Stub function for loading Inventory records into FOLIO"""
-    return True
+def move_marc_files(*args, **kwargs) -> list:
+    """Function moves MARC files to instances and holdings"""
+    marc_files = []
+    for path in pathlib.Path("/opt/airflow/symphony/").glob("*.*rc"):
+        if "holdings" in path.name:
+            target = pathlib.Path(f"/opt/airflow/migration/data/holdings/{path.name}")
+        else:
+            target = pathlib.Path(f"/opt/airflow/migration/data/instance/{path.name}")
+        shutil.move(path, target)
+        logger.info(f"Moved MARC file to {target}")
+        marc_files.append(str(target))
+    return marc_files
 
 
 def process_instances(*args, **kwargs) -> list:
-    """"Function for creating valid json from file of FOLIO instance objects"""
+    """ "Function for creating valid json from file of FOLIO instance objects"""
     instances = []
-    for file in pathlib.Path("/opt/airflow/migration/results").glob("folio_instance_*.json"):
+    for file in pathlib.Path("/opt/airflow/migration/results").glob(
+        "folio_instance_*.json"
+    ):
         with open(file) as fo:
             instances.extend([json.loads(i) for i in fo.readlines()])
 
@@ -73,7 +79,7 @@ with DAG(
         task_id="marc21_monitor",
         fs_conn_id="bib_path",
         filepath="/opt/airflow/symphony/*.*rc",
-        timeout=60 * 30
+        timeout=60 * 30,
     )
 
     monitor_file_mount.doc_md = dedent(
@@ -82,36 +88,34 @@ with DAG(
         Monitor's `/s/SUL/Dataload/Folio` for new MARC21 export files"""
     )
 
-    copy_marc_instance_files = BashOperator(
-        task_id="move_marc_file",
-        bash_command="mv /opt/airflow/symphony/*.marc /opt/airflow/migration/data/instance/"
+    move_marc = PythonOperator(
+        task_id="move_marc_files", python_callable=move_marc_files
     )
 
     convert_marc_to_folio = BashOperator(
         task_id="convert_marc_to_folio",
         bash_command="python /opt/airflow/MARC21-To-FOLIO/main_bibs.py --password $password --ils_flavour $ils_flavor --folio_version $folio_version --holdings_records False --force_utf_8 False --dates_from_marc False --hrid_handling default --suppress False /opt/airflow/migration $okapi_url $tenant $user",
-        env={ "folio_version": "iris",
+        env={
+            "folio_version": "iris",
             "ils_flavor": "001",
             "okapi_url": Variable.get("OKAPI_URL"),
             "password": Variable.get("FOLIO_PASSWORD"),
             "tenant": "sul",
-            "user": Variable.get("FOLIO_USER")
-        }
+            "user": Variable.get("FOLIO_USER"),
+        },
     )
 
     append_commas_to_file_lines = PythonOperator(
-        task_id="append_commas",
-        python_callable=process_instances
+        task_id="append_commas", python_callable=process_instances
     )
 
     post_to_folio = PythonOperator(
-        task_id="post_to_folio_instances",
-        python_callable=post_folio_instance_records
+        task_id="post_to_folio_instances", python_callable=post_folio_instance_records
     )
 
     archive_instance_files = BashOperator(
         task_id="archive_coverted_files",
-        bash_command="mv /opt/airflow/migration/data/instance/* /opt/airflow/migration/archive/.; mv /opt/airflow/migration/results/folio_instance_*.json /opt/airflow/migration/archive/."
+        bash_command="mv /opt/airflow/migration/data/instance/* /opt/airflow/migration/archive/.; mv /opt/airflow/migration/results/folio_instance_*.json /opt/airflow/migration/archive/.",
     )
 
     convert_marc_to_folio.doc_md = dedent(
@@ -125,6 +129,6 @@ with DAG(
         task_id="finish_loading",
     )
 
-    monitor_file_mount >> copy_marc_instance_files >> convert_marc_to_folio
+    monitor_file_mount >> move_marc >> convert_marc_to_folio
     convert_marc_to_folio >> append_commas_to_file_lines >> post_to_folio
     post_to_folio >> archive_instance_files >> finish_loading
--- dags/folio_post.py
+++ dags/folio_post.py
@@ -1,4 +1,3 @@
-
 import logging
 import requests
 import pathlib
@@ -26,8 +25,9 @@ def FolioLogin(**kwargs):
 
     result.raise_for_status()
 
+
 def _post_to_okapi(**kwargs):
-    endpoint = kwargs.get('endpoint')
+    endpoint = kwargs.get("endpoint")
     jwt = FolioLogin(**kwargs)
 
     records = kwargs["records"]
@@ -43,7 +43,7 @@ def _post_to_okapi(**kwargs):
         "x-okapi-tenant": tenant,
     }
 
-    payload = { "instances": records }
+    payload = {"instances": records}
 
     new_record_result = requests.post(
         okapi_instance_url,
@@ -56,13 +56,18 @@ def _post_to_okapi(**kwargs):
     if new_record_result.status_code > 399:
         logger.error(new_record_result.text)
         raise ValueError(
-            f"FOLIO POST Failed with error code:{new_record_result.status_code}")
+            f"FOLIO POST Failed with error code:{new_record_result.status_code}"
+        )
+
 
 def post_folio_instance_records(**kwargs):
     """Creates new records in FOLIO"""
     # instance_records = pathlib.Path('/tmp/instances.json').read_text()
     with open("/tmp/instances.json") as fo:
-      instance_records = json.load(fo)
+        instance_records = json.load(fo)
 
-    _post_to_okapi(records=instance_records,
-                   endpoint="/instance-storage/batch/synchronous?upsert=true", **kwargs)
+    _post_to_okapi(
+        records=instance_records,
+        endpoint="/instance-storage/batch/synchronous?upsert=true",
+        **kwargs,
+    )
