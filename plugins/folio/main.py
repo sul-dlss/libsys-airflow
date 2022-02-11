fece0076179dc4ff2dd5d64b79b52352deabd693
Jeremy Nelson <jermnelson@gmail.com>
2022-02-11 16:09:32 -0700

    Display FOLIO reports and logs by DAG Run ID

--- plugins/folio/main.py
+++ plugins/folio/main.py
@@ -1,3 +1,4 @@
+import csv
 import logging
 import pathlib
 
@@ -18,26 +19,61 @@ bp = Blueprint(
     static_url_path="/static/folio_plugin"
 )
 
+def _extract_dag_run_id(file_path: pathlib.Path) -> str:
+    """Extracts DAG ID run from file name, varies depending on file type"""
+    dag_run_id = None
+    path_parts = file_path.name.split("_")
+    if path_parts[0].startswith("transformation"):
+        dag_run_id = "_".join(path_parts[3:6])
+    if path_parts[0].startswith("failed_bib_records") and file_path.suffix == ".mrc":
+        dag_run_id = "_".join(path_parts[3:6])
+        # remove file extension
+        dag_run_id = dag_run_id.split(".")[0]
+    return dag_run_id
+
 class FOLIO(AppBuilderBaseView):
     default_view = "folio_view"
 
     @expose("/")
     def folio_view(self):
-        content = { }
-        for path in pathlib.Path(f"{MIGRATION_HOME}/reports").glob("*.md"):
-            reports.append(path.name)
-        return self.render_template("folio/index.html", content=reports)
+        content = {}
+        for path in pathlib.Path(f"{MIGRATION_HOME}/reports").iterdir():
+            dag_run_id = _extract_dag_run_id(path)
+            if not dag_run_id in content:
+                content[dag_run_id] = { "reports": [], "data_issues": [], "marc_errors": []}
+            if path.name.startswith("transformation"):
+                content[dag_run_id]["reports"].append(path.name)
+            if path.name.startswith("data_issues"):
+                content[dag_run_id]["data_issues"].append(path.name)
+        for path in pathlib.Path(f"{MIGRATION_HOME}/results").glob("failed_*.mrc"):
+            dag_run_id = _extract_dag_run_id(path)
+            if dag_run_id is None:
+                continue
+            content[dag_run_id]["marc_errors"].append( 
+                { "file": path.name, 
+                  "size": path.stat().st_size })
+        return self.render_template("folio/index.html", content=content)
 
-    @expose("/<report_name>")
+    @expose("/reports/<report_name>")
     def folio_report(self, report_name):
-        markdown_path = pathlib.Path(f"/opt/airflow/migration/reports/{report_name}")
-        rendered = markdown.markdown(markdown_path.read_text(), extensions=['md_in_html'])
+        markdown_path = pathlib.Path(f"{MIGRATION_HOME}/reports/{report_name}")
+        raw_text = markdown_path.read_text()
+        # Sets attribute to convert markdown in embedded HTML tags
+        final_mrkdown = raw_text.replace("<details>", """<details markdown="block">""")
+        rendered = markdown.markdown(final_mrkdown, extensions=['tables', 'md_in_html'])
         return self.render_template("folio/report.html", content=rendered)
 
-    @expose("/<log_name>")
+    @expose("/data_issues/<log_name>")
     def folio_data_issues(self, log_name):
-        tsv_path = pathlib
+        tsv_path = pathlib.Path(f"{MIGRATION_HOME}/results/{log_name}")
+        tsv_reader = csv.reader(tsv_path, delimiter="\t")
+        tsv_rows = [r for r in tsv_reader]
+        return self.render_template("folio/data-issues.html", content=tsv_rows)
 
+    @expose("/marc/<filename>")
+    def folio_marc_error(self, filename):
+        file_bytes = pathlib.Path(f"{MIGRATION_HOME}/results/{filename}").read_bytes()
+        return file_bytes
 
 
 v_appbuilder_view = FOLIO()
