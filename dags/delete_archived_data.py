04b2b46a3d83762e0a78e0b6661b3b66c94ee523
Joshua Greben <jgreben@stanford.edu>
2022-04-07 09:48:28 -0700

    DAG for deleting old archived data

---
+++ dags/delete_archived_data.py
@@ -0,0 +1,52 @@
+from datetime import datetime, timedelta
+import logging
+from pathlib import Path
+
+from airflow import DAG
+
+from airflow.operators.python import PythonOperator
+
+logger = logging.getLogger(__name__)
+
+default_args = {
+    "owner": "libsys",
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+
+def delete_archived_data(*args, **kwargs):
+    """Delete archived JSON Instances, Items, and Holdings"""
+    airflow = kwargs.get("airflow", "/opt/airflow")
+    airflow_path = Path(airflow)
+
+    archive_directory = airflow_path / "migration/archive"
+
+    for artifact in archive_directory.glob("*.json"):
+        modified = datetime.fromtimestamp(Path(artifact).stat().st_mtime)
+        yesterday = datetime.today() - timedelta(days=1)
+        if modified.date() >= yesterday.date():
+            try:
+                artifact.unlink()
+            except OSError as err:
+                logger.info(f"Cannot remove {artifact}: {err}")
+
+
+with DAG(
+    "remove_archived_migration_files",
+    default_args=default_args,
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2022, 1, 3),
+    catchup=False,
+    tags=["remove_archived"],
+) as dag:
+
+    archive_files = PythonOperator(
+        task_id="remove_archived_migration_files", python_callable=delete_archived_data
+    )
+
+
+archive_files
---
+++ plugins/tests/test_delete_archived_data.py
@@ -0,0 +1,28 @@
+import os
+from datetime import datetime, timedelta
+from pathlib import Path
+
+from dags.delete_archived_data import delete_archived_data
+from plugins.folio.helpers import archive_artifacts
+
+
+def test_delete_archived_data(mock_dag_run, mock_file_system):
+    dag = mock_dag_run
+    airflow_path = mock_file_system[0]
+    results_dir = mock_file_system[3]
+    archive_dir = mock_file_system[4]
+
+    # Create mock Instance JSON file
+    instance_filename = f"folio_instances_{dag.run_id}_bibs-transformer.json"
+    instance_file = results_dir / instance_filename
+    instance_file.write_text("""{ "id":"abcded2345"}""")
+
+    yesterday = datetime.timestamp(datetime.today() - timedelta(days=1))
+    os.utime(instance_file, times=(yesterday, yesterday))
+
+    archive_artifacts(dag_run=dag, airflow=airflow_path)
+
+    delete_archived_data(airflow=airflow_path)
+
+    files = Path(archive_dir).glob("**/*")
+    assert not (any(files))
