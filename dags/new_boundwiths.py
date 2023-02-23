bf9c12f875d699b5351e584566a5730555fc2af3
Jeremy Nelson <jermnelson@gmail.com>
2023-02-22 17:26:02 -0800

    Adds new Boundwith DAG and functions

---
+++ dags/new_boundwiths.py
@@ -0,0 +1,51 @@
+import logging
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.models import Variable
+from airflow.operators.python import PythonOperator
+
+from plugins.folio.helpers.bw import check_add_bw, discover_bw_parts_files
+
+logger = logging.getLogger(__name__)
+
+default_args = {
+    "owner": "folio",
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+parallel_posts = Variable.get("parallel_posts", 3)
+
+with DAG(
+    "boundwith_relationships",
+    default_args=default_args,
+    schedule_interval=None,
+    start_date=datetime(2023, 2, 22),
+    catchup=False,
+    tags=["bib_import", "folio"],
+    max_active_runs=1,
+) as dag:
+
+    discovery_bw_files = PythonOperator(
+        task_id="discovery-bw-parts",
+        python_callable=discover_bw_parts_files,
+        op_kwargs={"jobs": parallel_posts},
+    )
+
+    start_checks_add = EmptyOperator(task_id="start-check-add")
+
+    finished_checks_add = EmptyOperator(task_id="finished-check-add")
+
+    for i in range(int(parallel_posts)):
+        check_add_relationships = PythonOperator(
+            task_id=f"check-add-{i}", python_callable=check_add_bw, op_kwargs={"job": i}
+        )
+
+        start_checks_add >> check_add_relationships >> finished_checks_add
+
+    discovery_bw_files >> start_checks_add
---
+++ plugins/folio/helpers/bw.py
@@ -0,0 +1,82 @@
+import json
+import logging
+import pathlib
+
+import requests
+
+from airflow.models import Variable
+from folioclient import FolioClient
+
+logger = logging.getLogger(__name__)
+
+
+def discover_bw_parts_files(**kwargs):
+    airflow = kwargs.get("airflow", "/opt/airflow/")
+    jobs = int(kwargs["jobs"])
+    instance = kwargs["task_instance"]
+    iterations = pathlib.Path(airflow) / "migration/iterations"
+
+    bw_files = []
+    for iteration in iterations.iterdir():
+        bw_file = iteration / "results/boundwith_parts.json"
+        if bw_file.exists():
+            bw_files.append(str(bw_file))
+        else:
+            logger.error(f"{bw_file} doesn't exist")
+
+    shard_size = int(len(bw_files) / jobs)
+
+    for i in range(jobs):
+        start = i * shard_size
+        end = shard_size * (i + 1)
+        if i == jobs - 1:
+            end = len(bw_files)
+
+        instance.xcom_push(key=f"job-{i}", value=bw_files[start:end])
+
+    logger.info(f"Discovered {len(bw_files)} boundwidth part files for processing")
+
+
+def check_add_bw(**kwargs):
+    instance = kwargs["task_instance"]
+    folio_client = kwargs.get("folio_client")
+    job_number = kwargs["job"]
+
+    if folio_client is None:
+        folio_client = FolioClient(
+            Variable.get("OKAPI_URL"),
+            "sul",
+            Variable.get("FOLIO_USER"),
+            Variable.get("FOLIO_PASSWORD"),
+        )
+
+    bw_file_parts = instance.xcom_pull(
+        task_ids="discovery-bw-parts", key=f"job-{job_number}"
+    )
+
+    logger.info(f"Started processing {len(bw_file_parts)}")
+
+    total_bw, total_errors = 0, 0
+    for file_path in bw_file_parts:
+        logger.info(f"Starting Boundwith processing for {file_path}")
+        bw, errors = 0, 0
+        with open(file_path) as fo:
+            for line in fo.readlines():
+                record = json.loads(line)
+                post_result = requests.post(
+                    f"{folio_client.okapi_url}/inventory-storage/bound-with-parts",
+                    headers=folio_client.okapi_headers,
+                    json=record,
+                )
+                if post_result.status_code != 201:
+                    errors += 1
+                    logger.error(
+                        f"Failed to post {record['id']} from {file_path} error: {post_result.text}"
+                    )
+                bw += 1
+
+        logger.info(f"Processed {file_path} added {bw} errors {errors}")
+        total_errors += errors
+        total_bw += bw
+
+    logger.info(f"Finished added {total_bw:,} boundwidths with {total_errors:,} errors")
