94bf68c8ba4f5682f770fc59d46eef99606f6b62
Justin Littman <justinlittman@stanford.edu>
2023-04-10 07:38:05 -0400

    Adds method for getting interface credentials.
    refs #261

---
+++ plugins/folio/interface.py
@@ -0,0 +1,33 @@
+import logging
+
+from airflow.models import Variable
+
+from folioclient import FolioClient
+
+
+logger = logging.getLogger(__name__)
+
+
+def interface_info(**kwargs):
+    """
+    Retrieves the uri and credentials for an interface from the FOLIO API.
+    """
+    folio_client = kwargs.get("folio_client")
+
+    if folio_client is None:
+        folio_client = FolioClient(
+            Variable.get("OKAPI_URL"),
+            "sul",
+            Variable.get("FOLIO_USER"),
+            Variable.get("FOLIO_PASSWORD"),
+        )
+
+    interface_id = kwargs.get("interface_id")
+
+    interface_info = {}
+    interface_info['uri'] = folio_client.folio_get(f"organization-storage/interfaces/{interface_id}", key='uri')
+    credential_response = folio_client.folio_get(f"organization-storage/interfaces/{interface_id}/credentials")
+    interface_info['username'] = credential_response['username']
+    interface_info['password'] = credential_response['password']
+
+    return interface_info
---
+++ plugins/tests/test_interface.py
@@ -0,0 +1,39 @@
+import pydantic
+import pytest  # noqa
+
+from plugins.folio.interface import (
+    interface_info
+)
+
+
+class MockFOLIOClient(pydantic.BaseModel):
+    okapi_url: str = "https://okapi.edu"
+    okapi_headers: dict = {}
+    locations: list = []
+
+    def folio_get(self, path, key=None, query=""):
+        if path.endswith("/credentials"):
+            assert key is None
+            return credentials_response
+        else:
+            assert key == "uri"
+            return "ftps://www.gobi3.com"
+
+
+credentials_response = {
+    "id": "2552dadb-aa4e-4d58-ad99-f09384a53018",
+    "username": "my_user",
+    "password": "my_password",
+    "interfaceId": "588b5c42-8634-4af7-bc9b-5e0116ed96b6"
+}
+
+
+def test_interface_info():
+    folio_client = MockFOLIOClient()
+
+    interface_id = "588b5c42-8634-4af7-bc9b-5e0116ed96b6"
+    info = interface_info(interface_id=interface_id, folio_client=folio_client)
+
+    assert info['uri'] == "ftps://www.gobi3.com"
+    assert info['username'] == "my_user"
+    assert info['password'] == "my_password"
