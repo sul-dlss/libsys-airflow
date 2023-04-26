602a24e023a80e5327055a81f59a65c9268225d0
Justin Littman <justinlittman@stanford.edu>
2023-04-26 16:19:04 -0700

    Support FTP downloads.
    closes #250

---
+++ libsys_airflow/plugins/vendor/download.py
@@ -0,0 +1,68 @@
+import logging
+import re
+import pathlib
+from typing import Union
+
+from airflow.decorators import task
+from airflow.providers.ftp.hooks.ftp import FTPHook, FTPSHook
+
+logger = logging.getLogger(__name__)
+
+
+@task
+def ftp_download_task(
+    conn_id: str, remote_path: str, download_path: str, filename_regex: str
+) -> list[str]:
+    logger.info(f"Connection id is {conn_id}")
+
+    hook = _create_hook(conn_id)
+    return download(hook, remote_path, download_path, filename_regex)
+
+
+# Currently this only creates an FTPHook, but could be extended for SFTP and FTPS.
+def _create_hook(conn_id: str) -> FTPHook:
+    """Returns an FTPHook for the given connection id."""
+    hook = FTPHook(ftp_conn_id=conn_id)
+    [success, msg] = hook.test_connection()
+    if success:
+        return hook
+    else:
+        logger.error(f"Connection test result is {success}: {msg}")
+        raise Exception(msg)
+
+
+# In theory this can also be used for SFTP, but will require testing.
+def download(
+    hook: Union[FTPHook, FTPSHook],
+    remote_path: str,
+    download_path: str,
+    filename_regex: str,
+) -> list[str]:
+    """
+    Downloads files from FTP/FTPS and returns a list of file paths
+    """
+    all_filenames = hook.list_directory(remote_path)
+    logger.info(f"Found {len(all_filenames)} files in {remote_path}")
+    filtered_filenames = all_filenames
+    if filename_regex:
+        filtered_filenames = [
+            f for f in all_filenames if re.compile(filename_regex).match(f)
+        ]
+        logger.info(
+            f"Found {len(filtered_filenames)} files in {remote_path} with {filename_regex}"
+        )
+    # Remove already downloaded files
+    filtered_filenames = [
+        f
+        for f in filtered_filenames
+        if not pathlib.Path(_download_filepath(f, download_path)).is_file()
+    ]
+    for filename in filtered_filenames:
+        download_filepath = _download_filepath(filename, download_path)
+        hook.retrieve_file(filename, download_filepath)
+        logger.info(f"Downloaded {filename} to {download_filepath}")
+    return list(filtered_filenames)
+
+
+def _download_filepath(filename: str, download_path: str) -> str:
+    return f"{download_path}/{filename}"
--- libsys_airflow/plugins/vendor/marc_filter.py
+++ libsys_airflow/plugins/vendor/marc_filter.py
@@ -3,7 +3,17 @@ import pathlib
 
 import pymarc
 
+from airflow.decorators import task
+
 logger = logging.getLogger(__name__)
+
+
+@task
+def filter_fields_task(marc_path: str, fields: list):
+    """
+    Filters 905, 920, 986 from MARC records
+    """
+    filter_fields(pathlib.Path(marc_path), ["905", "920", "986"])
 
 
 def filter_fields(marc_path: pathlib.Path, fields: list):
@@ -12,13 +22,15 @@ def filter_fields(marc_path: pathlib.Path, fields: list):
     """
     logger.info(f"Filtering fields from {marc_path}")
     filtered_records = []
-    with marc_path.open('rb') as fo:
-        marc_reader = pymarc.MARCReader(fo, to_unicode=True, permissive=True, utf8_handling='replace')
+    with marc_path.open("rb") as fo:
+        marc_reader = pymarc.MARCReader(
+            fo, to_unicode=True, permissive=True, utf8_handling="replace"
+        )
         for record in marc_reader:
             record.remove_fields(*fields)
             filtered_records.append(record)
 
-    with marc_path.open('wb') as fo:
+    with marc_path.open("wb") as fo:
         marc_writer = pymarc.MARCWriter(fo)
         for record in filtered_records:
             record.force_utf8 = True
