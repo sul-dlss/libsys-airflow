d8eb1d97b38b7b801e4cf5133282576da50b581a
Aaron Collier <aaron.collier@stanford.edu>
2023-04-28 14:17:23 -0700

    Initial setup of vendor model

---
+++ libsys_airflow/plugins/vendor/models.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Text, Boolean
+from sqlalchemy.orm import declarative_base, relationship, Session
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+Model = declarative_base()
+
+class Vendor(Model):
+    __tablename__ = "vendors"
+
+    id = Column(Integer, primary_key=True)
+    display_name = Column(String(50), unique=True, nullable=False)
+    folio_organization_uuid = Column(String(36), unique=True, nullable=False)
+    vendor_code_from_folio = Column(String(36), unique=True, nullable=False)
+    acquisitions_unit_from_folio = Column(String(36), unique=False, nullable=False)
+    has_active_vendor_interfaces = Column(Boolean, nullable=False, default=False)
+    last_folio_update = Column(DateTime, nullable=False)
+
+    def __repr__(self) -> str:
+        return f"{self.name} - {self.uuid}"
+
+
+def dbinit():
+    pg_hook = PostgresHook("vendor_load")
+    Model.metadata.drop_all(pg_hook.get_sqlalchemy_engine())
+    Model.metadata.create_all(pg_hook.get_sqlalchemy_engine())
