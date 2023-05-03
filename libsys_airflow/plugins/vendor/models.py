39a134c8c65b8ab263eeda7c75b696cec65ca462
Naomi Dushay <ndushay@stanford.edu>
2023-05-03 13:19:25 -0700

    add vendor-interfaces model/table

--- libsys_airflow/plugins/vendor/models.py
+++ libsys_airflow/plugins/vendor/models.py
@@ -1,6 +1,5 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Text, Boolean
 from sqlalchemy.orm import declarative_base, relationship, Session
-from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 Model = declarative_base()
 
@@ -18,3 +17,22 @@ class Vendor(Model):
 
     def __repr__(self) -> str:
         return f"{self.display_name} - {self.folio_organization_uuid}"
+
+
+class VendorInterface(Model):
+    __tablename__ = "vendor_interfaces"
+
+    id = Column(Integer, primary_key=True)
+    vendor_id = Column(Integer, ForeignKey('vendors.id'))
+    display_name = Column(String(50), unique=False, nullable=False)
+    folio_interface_uuid = Column(String(36), unique=True, nullable=False)
+    folio_data_import_profile_uuid = Column(String(36), unique=True, nullable=False)
+    folio_data_import_processing_name = Column(String(50), unique=False, nullable=False)
+    file_pattern = Column(String(250), unique=False, nullable=True)
+    remote_path = Column(String(250), unique=False, nullable=True)
+    processing_dag = Column(String(50), unique=False, nullable=True)
+    processing_delay_in_days = Column(Integer, unique=False, nullable=True)
+    active = Column(Boolean, nullable=False, default=False)
+
+    def __repr__(self) -> str:
+        return f"{self.display_name} - {self.folio_interface_uuid}"
