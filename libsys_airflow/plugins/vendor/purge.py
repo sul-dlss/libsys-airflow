a98566a0655fd9f72384115c7019132226eb3e15
Jeremy Nelson <jermnelson@gmail.com>
2023-05-10 16:12:43 -0700

    Removes archive directories older than 90 days and captures contained information

--- libsys_airflow/plugins/vendor/purge.py
+++ libsys_airflow/plugins/vendor/purge.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 import logging
 import pathlib
+import shutil
 
 from airflow.decorators import task
 
@@ -24,6 +25,7 @@ def remove_records_task(target_directories: list) -> list:
     """
     Task take a list of paths and attempts to delete
     """
+    return remove_archived(target_directories)
 
 
 @task
@@ -33,14 +35,14 @@ def set_status_task(vendor_interfaces: list):
     """
 
 
-def find_directories(archive_directory: str) -> list:
+def find_directories(archive_directory: str, prior_days=90) -> list:
     """
-    Iteratates through archives to determine what vendor management
-    directories to be deleted
+    Iterates through archives to determine what vendor management
+    directories to delete based on age
     """
     target_dirs = []
     today = datetime.utcnow()
-    prior_90_days = (today - timedelta(days=90)).strftime("%Y%m%d")
+    prior_90_days = (today - timedelta(days=prior_days)).strftime("%Y%m%d")
     archive_directory = pathlib.Path(archive_directory)
     for directory in sorted(archive_directory.iterdir()):
         if directory.stem <= prior_90_days:
@@ -48,3 +50,32 @@ def find_directories(archive_directory: str) -> list:
     if len(target_dirs) < 1:
         logger.info("No directories available for purging")
     return target_dirs
+
+
+def remove_archived(archived_directories: list[str]) -> list[dict]:
+    """
+    Removes directories of archived vendor loads and returns a list of
+    Vendor/interface uuids for later updates
+    """
+    vendor_interfaces = []
+    for directory in archived_directories:
+        vendor_interfaces.append(_extract_uuids(directory))
+        shutil.rmtree(directory)
+    return vendor_interfaces
+
+
+def _extract_uuids(directory: str):
+    """
+    Extracts Vendor, VendorInterface, and Filename from Archive Directory
+    """
+    dir_path = pathlib.Path(directory)
+    output = {}
+    for vendor_path in dir_path.iterdir():
+        output[vendor_path.stem] = {
+            "date": dir_path.stem,
+            "interfaces": {}
+        }
+        for interface_path in vendor_path.iterdir():
+            for file in interface_path.iterdir():
+                output[vendor_path.stem]["interfaces"][interface_path.stem] = file.name
+    return output
--- tests/vendor/test_purge.py
+++ tests/vendor/test_purge.py
@@ -1,10 +1,27 @@
 from datetime import datetime, timedelta
-import pathlib
 
 import pytest  # noqa
 
 
-from libsys_airflow.plugins.vendor.purge import find_directories
+from libsys_airflow.plugins.vendor.purge import find_directories, remove_archived
+
+vendor_interfaces = [
+    {
+        "vendor": "8a8dc6dd-8be6-4bd9-80cd-e00409b37dc6",
+        "interfaces": {
+            "88d39c9c-fa8c-46ee-921d-71f725afb719": "ec1234.mrc",
+            "9666e9af-a203-4c38-8708-bda60af8f235": "abcd56679.mrc"
+        }
+    },
+    {
+        "vendor": "9cce436e-1858-4c37-9c7f-9374a36576ff",
+        "interfaces": {
+            "35a42dbe-399f-4292-b2d5-14dd9e0a5e39": "klio71923.mrc",
+            "65d30c15-a560-4064-be92-f90e38eeb351": "rt231.mrc"
+        }
+    }
+]
+
 
 @pytest.fixture
 def archive_basepath(tmp_path):
@@ -44,4 +61,26 @@ def test_find_empty_directory(archive_basepath, caplog):
     assert len(target_directories) == 0
 
 
+def test_remove_archived(archive_basepath):
+    # Create Mocks
+    today = datetime.utcnow()
+    prior_90 = today - timedelta(days=90)
+    target_directory = archive_basepath / prior_90.strftime("%Y%m%d")
+    target_directory.mkdir()
+    for row in vendor_interfaces:
+        vendor_path = target_directory / row["vendor"]
+        for interface, file in row["interfaces"].items():
+            interface_path = vendor_path / interface
+            interface_path.mkdir(parents=True)
+            (interface_path / file).touch()
 
+    target_directories = find_directories(archive_basepath)
+    result = remove_archived(target_directories)
+
+    assert target_directory.exists() is False
+    assert len(result[0]) == 2
+    first_vendor = result[0]["8a8dc6dd-8be6-4bd9-80cd-e00409b37dc6"]
+    assert first_vendor["date"] == prior_90.strftime("%Y%m%d")
+    assert len(first_vendor["interfaces"]) == 2
+    interface_marc_file = first_vendor["interfaces"]["88d39c9c-fa8c-46ee-921d-71f725afb719"]
+    assert interface_marc_file.startswith("ec1234.mrc")
