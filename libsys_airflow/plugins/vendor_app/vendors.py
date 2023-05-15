6abd091bf469498f6892cc473d759487a88fadc4
Ed Summers <ehs@pobox.com>
2023-05-15 08:45:20 -0400

    Add Vendor view
    Add a vendor view that lists the interfaces for a given vendor. The URL
    path is:
    
        /vendors/{vendor-id}
    
    Initially I had this as:
    
        /vendors/vendor/{vendor-id}
    
    But it looked kind of weird? We can change it later if it causes
    problems with subsequent views?
    
    I moved `tests.client` to `tests.airflow_client` to avoid confusion with
    other clients. I also modified the test_airflow_client response so that
    it can provide access to a BeautifulSoup object for the response to
    allow the tests to operate on the parsed HTML rather than doing straight
    up string matching.
    
    Closes #355

--- libsys_airflow/plugins/vendor_app/vendors.py
+++ libsys_airflow/plugins/vendor_app/vendors.py
@@ -1,7 +1,7 @@
 import logging
 
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-from flask_appbuilder import expose, BaseView as AppBuilderBaseView
+from flask_appbuilder import expose, BaseView
 from sqlalchemy.orm import Session
 
 from libsys_airflow.plugins.vendor.models import Vendor
@@ -10,19 +10,22 @@ from libsys_airflow.plugins.vendor.models import Vendor
 logger = logging.getLogger(__name__)
 
 
-class VendorManagementView(AppBuilderBaseView):
+class VendorManagementView(BaseView):
     default_view = "vendors_index"
     route_base = "/vendors"
 
-    def _get_vendors(self):
-        """
-        Retrieves vendors from vendor_loads database
-        """
-        pg_hook = PostgresHook("vendor_loads")
-        with Session(pg_hook.get_sqlalchemy_engine()) as session:
-            return session.query(Vendor).order_by(Vendor.display_name)
-
     @expose("/")
-    def vendors_index(self):
-        vendors = self._get_vendors()
+    def index(self):
+        session = self._get_session()
+        vendors = session.query(Vendor).order_by(Vendor.display_name)
         return self.render_template("vendors/index.html", vendors=vendors)
+
+    @expose("/<int:vendor_id>")
+    def vendor(self, vendor_id):
+        session = self._get_session()
+        vendor = session.query(Vendor).get(vendor_id)
+        return self.render_template("vendors/vendor.html", vendor=vendor)
+
+    def _get_session(self):
+        pg_hook = PostgresHook("vendor_loads")
+        return Session(pg_hook.get_sqlalchemy_engine())
