90341832a1e48aff16259451a759ad92d2b57c02
Laura Wrubel <lwrubel@stanford.edu>
2023-05-16 16:55:50 -0400

    Move marc fields

--- tests/vendor/test_marc.py
+++ tests/vendor/test_marc.py
@@ -3,10 +3,11 @@ import shutil
 import os
 import pathlib
 import logging
+import filecmp
 
 import pymarc
 
-from libsys_airflow.plugins.vendor.marc import filter_fields, batch
+from libsys_airflow.plugins.vendor.marc import filter_fields, batch, move_fields
 
 
 @pytest.fixture
@@ -40,3 +41,38 @@ def test_batch(tmp_path, marc_path):
         for record in marc_reader:
             count += 1
     assert count == 10
+
+
+@pytest.fixture
+def change_list():
+    return [{"from": "520", "to": "920"}, {"from": "504", "to": "904"}]
+
+
+@pytest.fixture
+def bad_change_list():
+    return [{"from": "520"}, {"from": "504", "to": "904"}]
+
+
+def test_move_fields(marc_path, change_list):
+    move_fields(marc_path, change_list)
+
+    with marc_path.open("rb") as fo:
+        marc_reader = pymarc.MARCReader(fo)
+        for record in marc_reader:
+            assert record.get_fields("520", "504") == []
+            if record.title() == "The loneliest whale blues /":
+                assert record.get_fields("920")
+            if (
+                record.title()
+                == "The FVN handbook : the principles and practices of the Fierce Vulnerability Network."
+            ):
+                assert record.get_fields("904")
+
+
+def test_move_fields_bad_changes(marc_path, bad_change_list, caplog, tmp_path):
+    caplog.set_level(logging.INFO)
+    original_file = shutil.copyfile(marc_path, os.path.join(tmp_path, "original.mrc"))
+    move_fields(marc_path, bad_change_list)
+
+    assert "Not moving fields." in caplog.text
+    assert filecmp.cmp(marc_path, original_file)
