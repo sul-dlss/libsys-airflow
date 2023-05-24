4aa561a3658b1abfda792669bbab16599277a919
Aaron Collier <aaron.collier@stanford.edu>
2023-05-24 13:17:21 -0700

    Run alembic merge heads

---
+++ vendor_loads_migration/versions/d4ec614edb35_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: d4ec614edb35
+Revises: 58a045522fe5, f63199d2c311
+Create Date: 2023-05-24 13:16:38.276262
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd4ec614edb35'
+down_revision = ('58a045522fe5', 'f63199d2c311')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
