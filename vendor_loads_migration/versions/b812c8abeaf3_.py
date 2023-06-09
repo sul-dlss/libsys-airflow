3e92233044c1bef501b668b147e8130c4d9ea7e2
Johnathan Martin <john.martin@stanford.edu>
2023-06-09 10:57:41 -0700

    run alembic merge heads

---
+++ vendor_loads_migration/versions/b812c8abeaf3_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: b812c8abeaf3
+Revises: 5dc5da2d1d3c, 697f20501d64
+Create Date: 2023-06-09 10:56:39.873807
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b812c8abeaf3'
+down_revision = ('5dc5da2d1d3c', '697f20501d64')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
