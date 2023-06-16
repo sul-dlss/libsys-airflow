fdc5bb2aed628f52d356417eada3d862ee5f0d4d
Jeremy Nelson <jermnelson@gmail.com>
2023-06-16 09:41:27 -0700

    Refactor to use TaskFlow, adds parallel processing for invoices

--- libsys_airflow/plugins/folio/invoices.py
+++ libsys_airflow/plugins/folio/invoices.py
@@ -1,18 +1,46 @@
-def _get_vouchers(folio_query, folio_client) -> list:
+import logging
+
+from airflow.decorators import task
+from airflow.models import Variable
+
+from libsys_airflow.plugins.folio.folio_client import FolioClient
+
+logger = logging.getLogger(__name__)
+
+
+def _folio_client():
+    try:
+        return FolioClient(
+            Variable.get("OKAPI_URL"),
+            "sul",
+            Variable.get("FOLIO_USER"),
+            Variable.get("FOLIO_PASSWORD"),
+        )
+    except ValueError as error:
+        logger.error(error)
+        raise
+
+
+def _get_ids_from_vouchers(folio_query: str, folio_client: FolioClient) -> list:
     """
-    Returns all voucher data given query parameter
+    Returns all invoice Ids from vouchers given query parameter
     """
-    return folio_client.folio_get(
+    vouchers = folio_client.get(
         "/voucher/vouchers", params={"query": folio_query, "limit": 100}
     )
+    return [row.get("invoiceId") for row in vouchers["vouchers"]]
 
 
-def invoices_awaiting_payment(folio_client):
-    _get_vouchers(
+@task
+def invoices_awaiting_payment_task() -> list:
+    folio_client = _folio_client()
+    invoice_ids = _get_ids_from_vouchers(
         "exportToAccounting=true and status=\"Awaiting payment\" and cql.allRecords=1 not disbursementNumber=\"\"",
         folio_client,
     )
+    return invoice_ids
 
 
-def invoices_pending_payment():
+@task
+def invoices_pending_payment_task(invoice_ids: list, upload_status: bool):
     return "foo"
