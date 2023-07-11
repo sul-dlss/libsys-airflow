450626b8d9cd69782a848222169a9c2946903a96
Jeremy Nelson <jermnelson@gmail.com>
2023-07-11 13:14:00 -0700

    Adds instatcode tsv handling to auto loads and helper

--- libsys_airflow/plugins/folio/helpers/__init__.py
+++ libsys_airflow/plugins/folio/helpers/__init__.py
@@ -85,6 +85,9 @@ def get_bib_files(**kwargs):
     task_instance.xcom_push(key="tsv-dates", value=bib_file_load["tsv-dates"])
     task_instance.xcom_push(key="mhld-file", value=bib_file_load.get("mhld"))
     task_instance.xcom_push(key="bwchild-file", value=bib_file_load.get("tsv-bwchild"))
+    task_instance.xcom_push(
+        key="tsv-instatcode", value=bib_file_load.get("tsv-instatcode")
+    )
 
 
 def post_to_okapi(**kwargs) -> dict:
