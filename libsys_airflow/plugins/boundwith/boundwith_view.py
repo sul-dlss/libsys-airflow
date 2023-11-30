2879e34949589166f94a3f6ec62401dce9558261
Jeremy Nelson <jermnelson@gmail.com>
2023-11-30 11:45:52 -0800

    Adds SUNID to UI

--- libsys_airflow/plugins/boundwith/boundwith_view.py
+++ libsys_airflow/plugins/boundwith/boundwith_view.py
@@ -12,7 +12,9 @@ from airflow.utils.state import State
 from flask_appbuilder import expose, BaseView as AppBuilderBaseView
 
 
-def trigger_bw_dag(bw_df: pd.DataFrame, user_email: Union[str, None]) -> str:
+def trigger_bw_dag(
+    bw_df: pd.DataFrame, sunid: str, user_email: Union[str, None]
+) -> str:
     dagbag = DagBag("/opt/airflow/dags")
     dag = dagbag.get_dag("add_bw_relationships")
     execution_date = timezone.utcnow()
@@ -21,8 +23,11 @@ def trigger_bw_dag(bw_df: pd.DataFrame, user_email: Union[str, None]) -> str:
         run_id=run_id,
         execution_date=execution_date,
         state=State.RUNNING,
-        conf={"relationships": bw_df.to_dict(orient='records'), 
-              "email": user_email},
+        conf={
+            "relationships": bw_df.to_dict(orient='records'),
+            "email": user_email,
+            "sunid": sunid,
+        },
         external_trigger=True,
     )
     return run_id, execution_date
@@ -34,21 +39,29 @@ class BoundWithView(AppBuilderBaseView):
 
     @expose("/create", methods=["POST"])
     def run_bw_creation(self):
+        sunid = request.form.get("sunid")
         if "upload-boundwith" not in request.files:
             flash("Missing Boundwith Relationship File")
+            rendered_page = self.render_template("boundwith/index.html")
+        elif len(sunid.strip()) < 1:
+            flash("SUNID Required")
             rendered_page = self.render_template("boundwith/index.html")
         else:
             raw_csv = request.files.get("upload-boundwith")
             email_addr = request.form.get("user-email")
+
             try:
                 bw_df = pd.read_csv(raw_csv)
                 if len(bw_df) > 1_000:
                     flash("Warning! CSV file has {len(bw_df)} rows, limit is 1,000")
                     rendered_page = self.render_template("boundwith/index.html")
                 else:
-                    run_id, execution_date = trigger_bw_dag(bw_df)
+                    run_id, execution_date = trigger_bw_dag(bw_df, email_addr, sunid)
                     rendered_page = self.render_template(
-                        "boundwith/index.html", run_id=run_id, execution_date=execution_date, user_email=email_addr
+                        "boundwith/index.html",
+                        run_id=run_id,
+                        execution_date=execution_date,
+                        user_email=email_addr,
                     )
             except pd.errors.EmptyDataError:
                 flash("Warning! Empty CSV file for Boundwith Relationship DAG")
