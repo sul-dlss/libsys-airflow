c47bd76af9b891e9c626fe43d2afc031262b17d2
Joshua Greben <jgreben@stanford.edu>
2024-02-22 14:18:06 -0800

    Format sql

--- libsys_airflow/plugins/data_exports/sql/instances_new.sql
+++ libsys_airflow/plugins/data_exports/sql/instances_new.sql
@@ -3,6 +3,7 @@ from sul_mod_inventory_storage.instance
 where (jsonb->>'statusId')::uuid in (
   select id
   from sul_mod_inventory_storage.instance_status
-  where jsonb->>'name' = 'Cataloged')
-  and jsonb->>'catalogedDate' = %s
-  and (jsonb->>'discoverySuppress')::boolean is false;
+  where jsonb->>'name' = 'Cataloged'
+)
+and jsonb->>'catalogedDate' = %s
+and (jsonb->>'discoverySuppress')::boolean is false;
