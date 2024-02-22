06aca3996529fdbb950ece5d76b35b8dd857d5eb
Joshua Greben <jgreben@stanford.edu>
2024-02-22 13:47:47 -0800

    Adds select sql for new instance records

---
+++ libsys_airflow/plugins/data_exports/sql/instances_new.sql
@@ -0,0 +1,8 @@
+select id
+from sul_mod_inventory_storage.instance
+where (jsonb->>'statusId')::uuid in (
+  select id
+  from sul_mod_inventory_storage.instance_status
+  where jsonb->>'name' = 'Cataloged')
+  and jsonb->>’catalogedDate’ = %s
+  and (jsonb->>’discoverySuppress’)::boolean is false;
