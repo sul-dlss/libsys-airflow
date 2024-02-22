46f7717f86c68aed6bef1bde7033e5882264a082
Joshua Greben <jgreben@stanford.edu>
2024-02-22 14:00:28 -0800

    Remove smart quotes from sql

--- libsys_airflow/plugins/data_exports/sql/instances_new.sql
+++ libsys_airflow/plugins/data_exports/sql/instances_new.sql
@@ -4,5 +4,5 @@ where (jsonb->>'statusId')::uuid in (
   select id
   from sul_mod_inventory_storage.instance_status
   where jsonb->>'name' = 'Cataloged')
-  and jsonb->>’catalogedDate’ = %s
-  and (jsonb->>’discoverySuppress’)::boolean is false;
+  and jsonb->>'catalogedDate' = %s
+  and (jsonb->>'discoverySuppress')::boolean is false;
