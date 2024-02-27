6777295fca692b92d3dc0b7e52ab0943c4459670
Shelley Doljack <sdoljack@stanford.edu>
2024-02-27 14:20:58 -0700

    Adds placeholder dags for sending data export records.

---
+++ libsys_airflow/dags/data_exports/west_transmission.py
@@ -0,0 +1,47 @@
+import logging
+from datetime import datetime, timedelta
+
+from airflow.decorators import dag
+from airflow.models import Variable
+from airflow.operators.empty import EmptyOperator
+
+from libsys_airflow.plugins.data_exports.transmission_tasks import (
+    connection_details_task,
+    gather_files_task,
+    transmit_data_task,
+)
+
+logger = logging.getLogger(__name__)
+
+default_args = {
+    "owner": "libsys",
+    "depends_on_past": False,
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=1),
+}
+
+
+@dag(
+    default_args=default_args,
+    schedule=timedelta(days=int(Variable.get("schedule_west_days", 1))),
+    start_date=datetime(2024, 1, 1),
+    catchup=False,
+    tags=["data export"],
+)
+def send_west_records():
+    start = EmptyOperator(task_id="start")
+
+    end = EmptyOperator(task_id="end")
+
+    gather_files = gather_files_task()
+
+    connection_details = connection_details_task()
+
+    transmit_data = transmit_data_task(connection_details)
+
+    start >> gather_files >> transmit_data >> end
+
+
+send_west_records()
