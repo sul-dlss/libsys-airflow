2f88a68e3e46ff43a4f6dfe14f451ebf6388ae3b
Shelley Doljack <sdoljack@stanford.edu>
2024-03-06 21:33:58 -0700

    Refactors gather files test for transmission tasks.

--- tests/data_exports/test_transmission_tasks.py
+++ tests/data_exports/test_transmission_tasks.py
@@ -8,11 +8,38 @@ from libsys_airflow.plugins.data_exports.transmission_tasks import (
 )
 
 
-def test_gather_files_task(tmp_path):
+# Mock xcom messages dict
+setup_marc_files = {
+    "marc_file_list": ["2024022914.mrc", "2024030114.mrc", "2024030214.mrc"],
+}
+
+
+@pytest.fixture
+def mock_file_system(tmp_path):
     airflow = tmp_path / "airflow"
-    data_exports = airflow / "data-export-files/oclc/marc-files/"
-    data_exports.mkdir(parents=True)
-    marc_file = data_exports / "2024022914.mrc"
-    marc_file.touch()
+    vendor_dir = airflow / "data-export-files/oclc/"
+    marc_file_dir = vendor_dir / "marc-files"
+    marc_file_dir.mkdir(parents=True)
+
+    archive_dir = vendor_dir / "archive"
+
+    return [airflow, marc_file_dir, archive_dir]
+
+
+@pytest.fixture
+def mock_marc_file_list(mock_file_system):
+    marc_file_dir = mock_file_system[1]
+    marc_file_list = []
+    for x in setup_marc_files['marc_file_list']:
+        marc_file = pathlib.Path(f"{marc_file_dir}/{x}")
+        marc_file.touch()
+        marc_file_list.append(str(marc_file))
+
+    return marc_file_list
+
+
+def test_gather_files_task(mock_file_system, mock_marc_file_list):
+    airflow = mock_file_system[0]
     marc_file_list = gather_files_task.function(airflow=airflow, vendor="oclc")
-    assert marc_file_list == [str(marc_file)]
+    assert marc_file_list.sort() == mock_marc_file_list.sort()
+
