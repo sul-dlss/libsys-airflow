f1229e78d263eafc719800a9001b231de0b3e2ff
Joshua Greben <jgreben@stanford.edu>
2024-03-14 11:47:42 -0700

    Include transmitted folder in download list

--- libsys_airflow/plugins/data_exports/apps/data_export_download_view.py
+++ libsys_airflow/plugins/data_exports/apps/data_export_download_view.py
@@ -8,6 +8,12 @@ vendor_file = open(parent / "vendors.json")
 vendors = json.load(vendor_file)
 
 
+def path_parent(full_path: pathlib.Path) -> str:
+    parts = full_path.parts
+
+    return parts[len(parts) - 2]
+
+
 class DataExportDownloadView(AppBuilderBaseView):
     default_view = "data_export_download_home"
     route_base = "/data_export_download"
@@ -19,13 +25,18 @@ class DataExportDownloadView(AppBuilderBaseView):
             for path in pathlib.Path(f"data-export-files/{vendor}/marc-files").glob(
                 "*"
             ):
-                content.append({vendor: path.name})
+                content.append({vendor: [path_parent(path), path.name]})
+
+            for path in pathlib.Path(f"data-export-files/{vendor}/transmitted").glob(
+                "*"
+            ):
+                content.append({vendor: [path_parent(path), path.name]})
 
         return self.render_template("data-export-download/index.html", content=content)
 
-    @expose("/downloads/<vendor>/<filename>")
-    def vendor_marc_record(self, vendor, filename):
+    @expose("/downloads/<vendor>/<folder>/<filename>")
+    def vendor_marc_record(self, vendor, folder, filename):
         file_bytes = pathlib.Path(
-            f"data-export-files/{vendor}/marc-files/{filename}"
+            f"data-export-files/{vendor}/{folder}/{filename}"
         ).read_bytes()  # noqa
         return file_bytes
