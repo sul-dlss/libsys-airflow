3aa28c13cd46af77989b27d5c3871a195e78b7d2
Joshua Greben <jgreben@stanford.edu>
2024-05-31 13:28:54 -0700

    Set schedule for removing archived files

--- libsys_airflow/dags/data_exports/remove-archived.py
+++ libsys_airflow/dags/data_exports/remove-archived.py
@@ -22,7 +22,7 @@ with DAG(
     "data_export_purge_archived_files",
     default_args=default_args,
     start_date=datetime(2024, 5, 30),
-    schedule=None,
+    schedule=timedelta(days=1),
     catchup=False,
     tags=["data export"],
 ) as dag:
