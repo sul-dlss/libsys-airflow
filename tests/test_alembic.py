a7926d7ef7b5a195ed5601918c14a2e78d6c855c
Jeremy Nelson <jermnelson@gmail.com>
2024-09-10 17:24:58 -0700

    refactor: Moves alembic test and add digital_bookplates test

--- tests/test_alembic.py
+++ tests/test_alembic.py
@@ -7,8 +7,10 @@ from alembic.script import ScriptDirectory
 def test_only_single_head_revision_in_migrations():
     config = Config('alembic.ini')
     vma_script = ScriptDirectory(dir=config.get_section('vma')['script_location'])
-    # digital_bookplate_script = ScriptDirectory(dir=config.get_section('digital_bookplates')['script_location'])
+    digital_bookplate_script = ScriptDirectory(
+        dir=config.get_section('digital_bookplates')['script_location']
+    )
 
     # This will raise if there are multiple heads
     vma_script.get_current_head()
-    # digital_bookplate_script.get_current_head()
+    digital_bookplate_script.get_current_head()
