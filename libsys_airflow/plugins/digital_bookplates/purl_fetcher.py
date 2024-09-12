01190a8d96d67528e4b3678ca90000bfe0b2d693
Jeremy Nelson <jermnelson@gmail.com>
2024-09-12 11:14:33 -0700

    feat: New extract_bookplate_metadata function and tests

--- libsys_airflow/plugins/digital_bookplates/purl_fetcher.py
+++ libsys_airflow/plugins/digital_bookplates/purl_fetcher.py
@@ -1,6 +1,13 @@
 import httpx
 
 from airflow.decorators import task
+from jsonpath_ng.ext import parse
+
+FUND_NAME_EXPR = parse(
+    "$.description.identifier[?(@.displayLabel == 'Symphony Fund Name')].value"
+)
+IMAGE_FILE_NAME_EXPR = parse("structural.contains[0].structural.contains[0].filename")
+TITLE_EXPR = parse("$.label")
 
 
 @task
@@ -16,8 +23,44 @@ def fetch_druids() -> list:
     return druids
 
 
+@task(max_active_tis_per_dag=10)
+def extract_bookplate_metadata(druid_url: str) -> dict:
+
+    druid = druid_url.split("/")[-1].split(".json")[0]
+
+    metadata = {
+        "druid": druid,
+        "image_file": None,
+        "failure": None,
+        "fund_name": None,
+        "title": None,
+    }
+    try:
+        druid_json = _get_druid_json(druid_url)
+        title = TITLE_EXPR.find(druid_json)
+        if len(title) > 0:
+            metadata['title'] = title[0].value
+        image_file = IMAGE_FILE_NAME_EXPR.find(druid_json)
+        if len(image_file) > 0:
+            metadata['image_file'] = image_file[0].value
+        else:
+            metadata["failure"] = "Missing image file"
+        fund_name = FUND_NAME_EXPR.find(druid_json)
+        if len(fund_name) > 0:
+            metadata["fund_name"] = fund_name[0].value
+    except httpx.HTTPStatusError as e:
+        metadata["failure"] = str(e)
+    return metadata
+
+
 def _child_druid(x):
     return x['druid']
+
+
+def _get_druid_json(druid_url: str) -> dict:
+    druid_result = httpx.get(druid_url, timeout=30)
+    druid_result.raise_for_status()
+    return druid_result.json()
 
 
 def _purls():
--- tests/digital_bookplates/test_purl_fetcher.py
+++ tests/digital_bookplates/test_purl_fetcher.py
@@ -1,6 +1,35 @@
+import httpx
 import pytest
 import requests_mock
-from libsys_airflow.plugins.digital_bookplates.purl_fetcher import fetch_druids
+
+
+from libsys_airflow.plugins.digital_bookplates.purl_fetcher import (
+    extract_bookplate_metadata,
+    fetch_druids,
+)
+
+
+sample_druids = {
+    "dk886cn6583": {  #
+        "druid": "druid:dk886cn6583",
+        "label": "The Fund for the Future of Libraries",
+        "description": {
+            "identifier": [{"displayLabel": "Symphony Fund Name", "value": "FUNDLIB"}]
+        },
+        "structural": {
+            "contains": [
+                {"structural": {"contains": [{"filename": "future_library.jp2"}]}}
+            ]
+        },
+    },
+    "vz805mb3388": {  # No image file
+        "druid": "druid:vz805mb3388",
+        "label": "In Memory of Samuel Clements, Gift of Tom Swift",
+        "description": {
+            "identifier": [{"displayLabel": "Symphony Fund Name", "value": "TWAIN"}]
+        },
+    },
+}
 
 
 @pytest.fixture
@@ -37,6 +66,24 @@ def mock_purl_fetcher_api_response():
     ]
 
 
+def mock_httpx_client():
+    def mock_response(request):
+        response = None
+        if request.url.path.endswith("vz805mb3388.json"):
+            response = httpx.Response(
+                status_code=200, json=sample_druids["vz805mb3388"]
+            )
+        if request.url.path.endswith("dk886cn6583.json"):
+            response = httpx.Response(
+                status_code=200, json=sample_druids["dk886cn6583"]
+            )
+        if response is None:
+            response = httpx.Response(status_code=404)
+        return response
+
+    return httpx.Client(transport=httpx.MockTransport(mock_response))
+
+
 def test_purl_fetch_list(mock_api, mocker, mock_purl_fetcher_api_response):
     mocker.patch(
         "libsys_airflow.plugins.digital_bookplates.purl_fetcher._purls",
@@ -48,3 +95,55 @@ def test_purl_fetch_list(mock_api, mocker, mock_purl_fetcher_api_response):
     assert len(test_druids) == 2
     assert test_druids[0] == "https://purl.stanford.edu/bm244yj4074.json"
     assert test_druids[1] == "https://purl.stanford.edu/bt942vy4674.json"
+
+
+def test_expected_druid(mocker):
+
+    mocker.patch(
+        "libsys_airflow.plugins.digital_bookplates.purl_fetcher.httpx.Client",
+        return_value=mock_httpx_client(),
+    )
+
+    druid_url = "https://purl.stanford.edu/ddk886cn6583.json"
+
+    metadata = extract_bookplate_metadata.function(druid_url)
+
+    assert metadata["title"].startswith("The Fund for the Future of Libraries")
+    assert metadata["fund_name"].startswith("FUNDLIB")
+    assert metadata["failure"] is None
+    assert metadata["image_file"].startswith("future_library.jp2")
+
+
+def test_missing_image_file(mocker):
+
+    mocker.patch(
+        "libsys_airflow.plugins.digital_bookplates.purl_fetcher.httpx.Client",
+        return_value=mock_httpx_client(),
+    )
+
+    druid_url = "https://purl.stanford.edu/vz805mb3388.json"
+
+    metadata = extract_bookplate_metadata.function(druid_url)
+
+    assert metadata["druid"] == "vz805mb3388"
+    assert metadata["title"].startswith(
+        "In Memory of Samuel Clements, Gift of Tom Swift"
+    )
+    assert metadata["fund_name"].startswith("TWAIN")
+    assert metadata["failure"].startswith("Missing image file")
+
+
+def test_missing_purl(mocker):
+
+    mocker.patch(
+        "libsys_airflow.plugins.digital_bookplates.purl_fetcher.httpx.Client",
+        return_value=mock_httpx_client(),
+    )
+
+    druid_url = "https://purl.stanford.edu/pn034fs6161.json"
+
+    metadata = extract_bookplate_metadata.function(druid_url)
+
+    assert metadata['druid'] == "pn034fs6161"
+
+    assert metadata['failure'].startswith("Client error '404 Not Found'")
