8423b1bb00e211971897c45d8488e917a1b7dbcd
Joshua Greben <jgreben@stanford.edu>
2024-09-18 15:48:06 -0700

    Task to return liat of uuids for folio funds matching digital_bookplates table funds

--- libsys_airflow/plugins/digital_bookplates/bookplates.py
+++ libsys_airflow/plugins/digital_bookplates/bookplates.py
@@ -1,19 +1,51 @@
 import logging
 
 from airflow.decorators import task
+from airflow.models import Variable
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from sqlalchemy.orm import Session
+from libsys_airflow.plugins.digital_bookplates.models import DigitalBookplate
+from folioclient import FolioClient
 
 logger = logging.getLogger(__name__)
 
 
+def _folio_client():
+    return FolioClient(
+        Variable.get("OKAPI_URL"),
+        "sul",
+        Variable.get("FOLIO_USER"),
+        Variable.get("FOLIO_PASSWORD"),
+    )
+
+
 @task
-def bookplate_fund_ids(**kwargs) -> dict:
+def bookplate_fund_ids(**kwargs) -> list:
     """
     Looks up in bookplates table for fund_name
     Queries folio for fund_name
     Returns list of fund UUIDs
     """
-    # {"druid": "fund_uuid"}
-    funds: dict = {}
+    folio_client = _folio_client()
+    folio_funds = folio_client.folio_get(
+        "/finance-storage/funds", query_params={"limit": 2999}
+    )
+
+    pg_hook = PostgresHook("digital_bookplates")
+    with Session(pg_hook.get_sqlalchemy_engine()) as session:
+        fund_tuples = (
+            session.query(DigitalBookplate.fund_name).where(
+                DigitalBookplate.fund_name.is_not(None)
+            )
+        ).all()
+
+    funds: list = []
+    fund_names = [n[0] for n in fund_tuples]
+
+    for fund in folio_funds['funds']:
+        if fund['name'] in fund_names:
+            funds.append(fund['id'])
+
     return funds
 
 
