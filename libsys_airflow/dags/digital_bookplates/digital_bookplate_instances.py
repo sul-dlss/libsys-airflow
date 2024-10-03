287768f0dcf250b9af8ebcdc475f5b4b4b14bf37
Shelley Doljack <sdoljack@stanford.edu>
2024-10-03 17:24:11 -0600

    Updates DAG variable name.

--- libsys_airflow/dags/digital_bookplates/digital_bookplate_instances.py
+++ libsys_airflow/dags/digital_bookplates/digital_bookplate_instances.py
@@ -47,9 +47,9 @@ def digital_bookplate_instances():
 
     retrieve_paid_invoice_lines = invoice_lines_from_invoices(retrieve_paid_invoices)
 
-    filter_invoice_lines = filter_invoice_lines(retrieve_paid_invoice_lines)
+    filter_paid_invoice_lines = filter_invoice_lines(retrieve_paid_invoice_lines)
 
-    retrieve_bookplate_fund_po_lines = bookplate_fund_po_lines(filter_invoice_lines)
+    retrieve_bookplate_fund_po_lines = bookplate_fund_po_lines(filter_paid_invoice_lines)
 
     retrieve_instances = instances_from_po_lines(retrieve_bookplate_fund_po_lines)
 
