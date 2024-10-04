d0b58b2907f350c0b0da2acc7ffb179d61fc8d08
Shelley Doljack <sdoljack@stanford.edu>
2024-10-04 16:33:51 -0600

    Adds bookplate_fund_po_lines task and test.

--- libsys_airflow/plugins/digital_bookplates/bookplates.py
+++ libsys_airflow/plugins/digital_bookplates/bookplates.py
@@ -19,6 +19,31 @@ def _folio_client():
     )
 
 
+def _get_bookplate_metadata_with_fund_uuids() -> dict:
+    funds = {}
+    pg_hook = PostgresHook("digital_bookplates")
+    with Session(pg_hook.get_sqlalchemy_engine()) as session:
+        for f in (
+            session.query(
+                DigitalBookplate.fund_uuid,
+                DigitalBookplate.fund_name,
+                DigitalBookplate.druid,
+                DigitalBookplate.image_filename,
+                DigitalBookplate.title,
+            )
+            .where(DigitalBookplate.fund_uuid.is_not(None))
+            .all()
+        ):
+            funds[f.fund_uuid] = {
+                "fund_name": f.fund_name,
+                "druid": f.druid,
+                "image_filename": f.image_filename,
+                "title": f.title,
+            }
+
+    return funds
+
+
 @task
 def bookplate_fund_ids(**kwargs) -> dict:
     """
@@ -55,8 +80,9 @@ def bookplate_fund_ids(**kwargs) -> dict:
 def bookplate_fund_po_lines(invoice_lines: list) -> list:
     """
     Checks if fund Id from invoice lines data struct contains bookplate fund
-    This task needs to lookup in the digital bookplates table the fund_id
-    and remove from the list objects that are not in the bookplates table
+    This task needs to get digital bookplates data from the table and
+    return a list of bookplates metadata and poline ids
+    Input data struct:
     [
       {
         "fadacf66-8813-4759-b4d3-7d506db38f48": {
@@ -67,11 +93,41 @@ def bookplate_fund_po_lines(invoice_lines: list) -> list:
         }
       }
     ]
-    Returns po lines and fund ids that are bookplate funds
+    Returns:
+    [
+      {
+        "bookplate_metadata": { "druid": "", "fund_name": "", "image_filename": "", "title": "" },
+        "poline_id": "798596da-12a6-4c6d-8d3a-3bb6c54cb2f1"
+      },
+      {
+        "bookplate_metadata": { "druid": "", "fund_name": "", "image_filename": "", "title": "" },
+        "poline_id": "7a5888fe-689b-4cfe-a27d-c2675a235203"
+      }
+    ]
     """
-    funds_invoice_lines: list = []
+    bookplates_polines: list = []
+    bookplates = _get_bookplate_metadata_with_fund_uuids()
+    # create new list of funds and poline objects (removes invoice lines and empty values)
+    funds_polines = []
+    for row in invoice_lines:
+        for k, v in row.items():
+            if row[k]:
+                funds_polines.append(v)
 
-    return funds_invoice_lines
+    # check for fund id in bookplates dict and add poline id to new dict
+    for row in funds_polines:
+        funds = row.get("fund_ids")
+        poline_id = row.get("poline_id")
+        bp_poline_dict = {}
+        for f in funds:
+            md = bookplates.get(f)
+            if f and md is not None:
+                bp_poline_dict.update({"bookplate_metadata": md})
+                bp_poline_dict.update({"poline_id": poline_id})
+
+        bookplates_polines.append(bp_poline_dict)
+
+    return bookplates_polines
 
 
 @task
