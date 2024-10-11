1237affd570902f6a5f6407d35a363d4149dda9d
Jeremy Nelson <jermnelson@gmail.com>
2024-10-11 16:07:17 -0700

    feat: Saves uploaded file to volume and triggers add 979 DAG

--- libsys_airflow/plugins/digital_bookplates/apps/digital_bookplates_batch_upload_view.py
+++ libsys_airflow/plugins/digital_bookplates/apps/digital_bookplates_batch_upload_view.py
@@ -1,7 +1,14 @@
+import datetime
 import logging
+import pathlib
+
 import pandas as pd
 
+from airflow.models import DagBag
 from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.utils import timezone
+from airflow.utils.state import State
+
 from flask import flash, redirect, request
 from flask_appbuilder import expose, BaseView as AppBuilderBaseView
 from sqlalchemy.orm import Session
@@ -12,12 +19,43 @@ from libsys_airflow.plugins.digital_bookplates.models import DigitalBookplate
 logger = logging.getLogger(__name__)
 
 
-def _trigger_add_979_dags(**kwargs):
-    """Placeholder to trigger DAG"""
+def _trigger_add_979_dags(**kwargs) -> str:
+    """Triggers add_bookplate_970 DAG"""
     instance_uuid = kwargs["instance_uuid"]
     fund = kwargs["fund"]
     dag_payload = {instance_uuid: fund}
-    logger.info(f"Triggers 979 DAG with payload {dag_payload}")
+    dagbag = DagBag("/opt/airflow/dags")
+    dag = dagbag.get_dag("digital_bookplate_979")
+
+    execution_date = timezone.utcnow()
+    run_id = f"manual__{execution_date.isoformat()}"
+    dag.create_dagrun(
+        run_id=run_id,
+        execution_date=execution_date,
+        state=State.RUNNING,
+        conf={"druids_for_instance_id": dag_payload},
+        external_trigger=True,
+    )
+    logger.info(f"Triggers 979 DAG with dag_id {run_id}")
+    return run_id
+
+
+def _save_uploaded_file(files_base: str, file_name: str, upload_df: pd.DataFrame):
+    """
+    Saves uploaded file to digital-bookplates/{year}/{day} location
+    and if file name already exists, increments until unique
+    """
+    current_time = datetime.datetime.utcnow()
+    report_path = (
+        pathlib.Path(files_base) / f"{current_time.year}/{current_time.day}/{file_name}"
+    )
+    count: int = 1
+    while report_path.exists():
+        report_path = report_path.with_name(
+            f"{report_path.stem}-{count}{report_path.suffix}"
+        )
+        count += 1
+    upload_df.to_csv(report_path, index=False)
 
 
 def _get_fund(fund_id: int) -> dict:
@@ -44,10 +82,18 @@ class DigitalBookplatesBatchUploadView(AppBuilderBaseView):
         raw_upload_instances_file = request.files["upload-instances-file"]
         try:
             upload_instances_df = pd.read_csv(raw_upload_instances_file, header=None)
+            dag_runs = []
             for row in upload_instances_df.iterrows():
                 instance_uuid = row[1]
-                _trigger_add_979_dags(instance_uuid=instance_uuid, fund=fund)
-
+                dag_run_id = _trigger_add_979_dags(
+                    instance_uuid=instance_uuid, fund=fund
+                )
+                dag_runs.append(dag_run_id)
+            _save_uploaded_file(
+                DigitalBookplatesBatchUploadView.files_base,
+                raw_upload_instances_file.filename,
+                upload_instances_df,
+            )
         except pd.errors.EmptyDataError:
             flash("Warning! Empty Instance UUID file.")
             return redirect('/digital_bookplates_batch_upload')
