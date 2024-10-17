63363bd9fa9dd60667aa4dc6178d58dfcf807e9a
Jeremy Nelson <jermnelson@gmail.com>
2024-10-17 13:42:42 -0700

    feat: Polling for 979 DAGs DAG

--- libsys_airflow/dags/digital_bookplates/poll_for_979s.py
+++ libsys_airflow/dags/digital_bookplates/poll_for_979s.py
@@ -15,20 +15,25 @@ default_args = {
     "retry_delay": timedelta(minutes=1),
 }
 
+
 @task(multiple_outputs=True)
 def retrieve_variables(**kwargs):
     params = kwargs['params']
     dag_runs = params.get("dag_runs")
     addl_email = params.get("email")
-    return { "dag_runs": dag_runs, "email": addl_email }
+    return {"dag_runs": dag_runs, "email": addl_email}
 
 
 @task
 def poll_979_dags(**kwargs):
     dag_runs = kwargs.get('dag_runs', [])
     # Checks every 10 seconds
-    sensor = DAG979Sensor(task_id="poll-979-dags", dag_runs=dag_runs, poke_interval=10.0).execute(kwargs)
+    sensor = DAG979Sensor(
+        task_id="poll-979-dags", dag_runs=dag_runs, poke_interval=10.0
+    )
+    sensor.execute(kwargs)
     return sensor.dag_runs
+
 
 @dag(
     default_args=default_args,
@@ -45,8 +50,8 @@ def poll_for_digital_bookplate_979s():
 
     poll_result = poll_979_dags(dag_runs=vars["dag_runs"])
 
-    start >> vars 
-    
+    start >> vars
+
     poll_result >> end
 
 
