962d12ab8358a56273069206b7e98b815843b0ad
Jeremy Nelson <jermnelson@gmail.com>
2024-10-17 13:42:00 -0700

    feat: Custom sensor for polling add 979 tags DAGs

--- libsys_airflow/plugins/digital_bookplates/dag_979_sensor.py
+++ libsys_airflow/plugins/digital_bookplates/dag_979_sensor.py
@@ -1,9 +1,10 @@
 import logging
 
-from airflow.models import DAG, DagRun
+from airflow.models import DagRun
 from airflow.sensors.base_sensor_operator import BaseSensorOperator
 
 logger = logging.getLogger(__name__)
+
 
 class DAG979Sensor(BaseSensorOperator):
 
@@ -14,15 +15,13 @@ class DAG979Sensor(BaseSensorOperator):
         super().__init__(**kwargs)
 
     def poke(self, context) -> bool:
-        logger.info("In POKE for DAG 979")
-        dag = DAG(dag_id='digital_bookplate_979')
-        last_dag_run = dag.get_last_dagrun(include_externally_triggered=True)
-        logger.info(f"{dag} DAG {dag.has_dag_runs()} last run is {last_dag_run.run_id}")
-        for i,dag_run_id in enumerate(self.dag_runs.keys()):
-            dag_run = DagRun(
-                dag_id='digital_bookplate_979',
-                run_id=last_dag_run.run_id)
-            logger.info(f"{dag_run_id} status is {dag_run.state} {dag_run}")
+        logger.info(f"Checking state for {len(self.dag_runs)} DAG runs")
+        for dag_run_id in self.dag_runs.keys():
+            dag_runs = DagRun.find(dag_id='digital_bookplate_979', run_id=dag_run_id)
+            if len(dag_runs) < 1:
+                continue
+            dag_run = dag_runs[0]
             self.dag_runs[dag_run_id] = dag_run.get_state()
-        return all([val in ['SUCCESS', 'FAILED'] for val in self.dag_runs.values()])
-
+        poke_result = all([val in ['success', 'failed'] for val in self.dag_runs.values()])
+        logger.info(f"Result of polling DAGs {poke_result}")
+        return poke_result
