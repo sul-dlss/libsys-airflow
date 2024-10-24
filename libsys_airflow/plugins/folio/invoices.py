105c2446536f2bebebac05e7dc78b18431013e4a
Joshua Greben <jgreben@stanford.edu>
2024-10-24 13:45:43 -0700

    Adds comments with function inputs and output direction

--- libsys_airflow/plugins/folio/invoices.py
+++ libsys_airflow/plugins/folio/invoices.py
@@ -171,6 +171,20 @@ def invoice_lines_paid_on_fund(**kwargs) -> list:
 
 @task.branch()
 def date_range_or_funds_path(**kwargs):
+    # params["funds"]:
+    """
+    [
+        {
+            'druid': 'ef919yq2614',
+            'failure': None,
+            'fund_name': 'KELP',
+            'title': 'The Kelp Foundation Fund',
+            'image_filename': 'ef919yq2614_00_0001.jp2',
+            'db_id': 4,
+            'fund_uuid': 'f916c6e4-1bc7-4892-a5a8-73b8ede6e3a4'
+        },
+    ]
+    """
     params = kwargs.get("params", {})
     funds = params.get("funds", [])
     if len(funds) > 0:
