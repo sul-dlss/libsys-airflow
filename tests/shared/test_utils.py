587420b5a3e2abbedd96250705ad1bd25af75495
Shelley Doljack <sdoljack@stanford.edu>
2024-10-28 22:11:14 -0600

    Refactors adding tags so that new tags are appended.

--- tests/shared/test_utils.py
+++ tests/shared/test_utils.py
@@ -1,5 +1,6 @@
 import json
 import httpx
+import pymarc
 import pytest
 
 from jsonpath_ng.ext import parse
@@ -52,12 +53,28 @@ def marc_json():
 
 
 @pytest.fixture
+def marc_979():
+    return {
+        "979": {
+            "ind1": " ",
+            "ind2": " ",
+            "subfields": [
+                {"f": "ABBOTT"},
+                {"b": "druid:ws066yy0421"},
+                {"c": "ws066yy0421_00_0001.jp2"},
+                {"d": "The Donald P. Abbott Fund for Marine Invertebrates"},
+            ],
+        }
+    }
+
+
+@pytest.fixture
 def marc_instance_tags():
     return {
         "979": [
             {
-                "ind1": "",
-                "ind2": "",
+                "ind1": " ",
+                "ind2": " ",
                 "subfields": [
                     {"f": "ABBOTT"},
                     {"b": "druid:ws066yy0421"},
@@ -65,6 +82,34 @@ def marc_instance_tags():
                     {"d": "The Donald P. Abbott Fund for Marine Invertebrates"},
                 ],
             }
+        ]
+    }
+
+
+@pytest.fixture
+def marc_instance_two_tags():
+    return {
+        "979": [
+            {
+                "ind1": " ",
+                "ind2": " ",
+                "subfields": [
+                    {"f": "STEINMETZ"},
+                    {"b": "druid:nc092rd1979"},
+                    {"c": "nc092rd1979_00_0001.jp2"},
+                    {"d": "Verna Pace Steinmetz Endowed Book Fund in History"},
+                ],
+            },
+            {
+                "ind1": " ",
+                "ind2": " ",
+                "subfields": [
+                    {"f": "WHITEHEAD"},
+                    {"b": "druid:ph944pq1002"},
+                    {"c": "ph944pq1002_00_0001.jp2"},
+                    {"d": "Barry Whitehead Memorial Book Fund"},
+                ],
+            },
         ]
     }
 
@@ -87,50 +132,15 @@ def mock_folio_add_marc_tags(mocker):
 def mock_httpx_client():
     def mock_response(request):
         response = None
-        # following is not used in test but leaving here for testing more parts of utils.py
-        # match request.method:
-
-        #     case 'PUT':
-        #         if request.url.path.startswith('/change-manager/parsedRecords'):
-        #             response = httpx.Response(status_code=202)
-
         return response
 
     return httpx.Client(transport=httpx.MockTransport(mock_response))
 
 
 def mock_folio_client(mocker):
-    # following is not used in test but leaving here for testing more parts of utils.py
-    # def __srs_response__(path: str):
-    #     output = {}
-    #     instance_uuid = path.split("instanceId=")[-1]
-
-    #     match instance_uuid:
-    #         case "06660d4f-982d-54e8-b34c-532c268868e1":
-    #             output = {
-    #                 "sourceRecords": [
-    #                     {
-    #                         "recordId": "e60b77d3-3a76-59e2-88f7-3d1a045af3b1",
-    #                         "parsedRecord": {"content": marc_json},
-    #                     }
-    #                 ]
-    #             }
-
-    #     return output
 
     def mock_folio_get(*args, **kwargs):
         output = {}
-        # following is not used in test but leaving here for testing more parts of utils.py
-        # if args[0].startswith("/source-storage/source-records"):
-        #     output = __srs_response__(args[0])
-        # if args[0].startswith("/inventory/instances/"):
-        #     for instance_uuid in [
-        #         "64a5a15b-d89e-4bdd-bbd6-fcd215b367e4",
-        #         "242c6000-8485-5fcd-9b5e-adb60788ca59",
-        #     ]:
-        #         if args[0].endswith(instance_uuid):
-        #             output = {"_version": "1", "hrid": "a123456"}
-
         return output
 
     mock = mocker
@@ -141,7 +151,7 @@ def mock_folio_client(mocker):
 
 
 def test__marc_json_with_new_tags__(
-    mock_folio_add_marc_tags, marc_json, marc_instance_tags
+    mock_folio_add_marc_tags, marc_json, marc_instance_tags, caplog
 ):
     add_marc_tag = utils.FolioAddMarcTags()
     marc_json_with_new_tags = add_marc_tag.__marc_json_with_new_tags__(
@@ -151,3 +161,65 @@ def test__marc_json_with_new_tags__(
     tag_979_exp = parse("$.fields[?(@['979'])]")
     tag_979 = tag_979_exp.find(new_record_dict)[0].value
     assert len(tag_979["979"]["subfields"]) == 4
+    assert "New field 979 is unique tag." in caplog.text
+
+
+def test__marc_json_with_two_new_tags__(
+    mock_folio_add_marc_tags, marc_json, marc_instance_two_tags, caplog
+):
+    add_marc_tag = utils.FolioAddMarcTags()
+    marc_json_with_new_tags = add_marc_tag.__marc_json_with_new_tags__(
+        marc_json, marc_instance_two_tags
+    )
+    new_record_dict = json.loads(marc_json_with_new_tags)
+    tag_979_exp = parse("$.fields[?(@['979'])]")
+    new_979_tags = tag_979_exp.find(new_record_dict)
+    assert len(new_979_tags) == 2
+    assert (
+        "Record does not have existing 979's. New fields will be added." in caplog.text
+    )
+
+
+def test__marc_json_existing_tags__(
+    mock_folio_add_marc_tags, marc_json, marc_979, marc_instance_tags, caplog
+):
+    add_marc_tag = utils.FolioAddMarcTags()
+    marc_json["fields"].append(marc_979)
+    marc_json_with_new_tags = add_marc_tag.__marc_json_with_new_tags__(
+        marc_json, marc_instance_tags
+    )
+    new_record_dict = json.loads(marc_json_with_new_tags)
+    tag_979_exp = parse("$.fields[?(@['979'])]")
+    new_979_tags = tag_979_exp.find(new_record_dict)
+    assert len(new_979_tags) == 1
+    assert (
+        "Record has existing 979's. New fields will be evaluated for uniqueness."
+        in caplog.text
+    )
+    assert (
+        "Skip adding duplicated ABBOTT druid:ws066yy0421 ws066yy0421_00_0001.jp2 The Donald P. Abbott Fund for Marine Invertebrates field"
+        in caplog.text
+    )
+    assert "New field 979 is not unique" in caplog.text
+
+
+def test__tag_is_unique__(mock_folio_add_marc_tags, marc_json):
+    add_marc_tag = utils.FolioAddMarcTags()
+    reader = pymarc.reader.JSONReader(json.dumps(marc_json))
+    for record in reader:
+        existing_tags = record.get_fields("979")
+
+    new_field = pymarc.Field(
+        tag="979",
+        indicators=[" ", " "],
+        subfields=[
+            pymarc.Subfield(code='f', value='ABBOTT'),
+            pymarc.Subfield(code='b', value='druid:ws066yy0421'),
+            pymarc.Subfield(code='c', value='ws066yy0421_00_0001.jp2'),
+            pymarc.Subfield(
+                code='d',
+                value='The The Donald P. Abbott Fund for Marine Invertebrates',
+            ),
+        ],
+    )
+    assert add_marc_tag.__tag_is_unique__(existing_tags, new_field) is True
