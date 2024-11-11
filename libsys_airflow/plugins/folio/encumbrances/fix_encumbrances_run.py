1447cdfc6d589e8ce197f3dc286e173ee852a3b4
Joshua Greben <jgreben@stanford.edu>
2024-11-11 12:58:44 -0800

    Output logger to file

--- libsys_airflow/plugins/folio/encumbrances/fix_encumbrances_run.py
+++ libsys_airflow/plugins/folio/encumbrances/fix_encumbrances_run.py
@@ -1,5 +1,6 @@
 import asyncio
 import contextlib
+import logging
 import pathlib
 import libsys_airflow.plugins.folio.encumbrances.fix_encumbrances as fix_encumbrances_script
 
@@ -18,8 +19,10 @@ def fix_encumbrances_run(*args, **kwargs):
     log_path = pathlib.Path(airflow) / f"fix_encumbrances/{library}-{run_id}.log"
     log_path.parent.mkdir(parents=True, exist_ok=True)
 
+    
     with log_path.open("w+", 1) as log:
-        with contextlib.redirect_stdout(log):
+        logging.basicConfig(level=logging.INFO, filename=log)
+        with contextlib.redirect_stdout(OutputLogger()):
             asyncio.run(
                 fix_encumbrances_script.run_operation(
                     int(choice), fiscal_year_code, tenant, username, password
@@ -27,3 +30,16 @@ def fix_encumbrances_run(*args, **kwargs):
             )
 
     return str(log_path.absolute())
+
+
+class OutputLogger:
+    def __init__(self, name="root", level="INFO"):
+        self.logger = logging.getLogger(name)
+        self.level = getattr(logging, level)
+
+    def write(self, msg):
+        if msg and not msg.isspace():
+            self.logger.log(self.level, msg)
+
+    def flush(self):
+        pass
