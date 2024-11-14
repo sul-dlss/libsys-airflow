bc30a9af893bc561de8dcad903cd98a83fc78757
Shelley Doljack <sdoljack@stanford.edu>
2024-11-14 09:50:02 -0700

    Revert "Updates trigger_digital_bookplate_979_task to process incoming data that is list of lists."
    This reverts commit c6742fbf4d6d2638369beb2b2c89a2ee875cc5aa.

--- libsys_airflow/plugins/digital_bookplates/bookplates.py
+++ libsys_airflow/plugins/digital_bookplates/bookplates.py
@@ -283,22 +283,16 @@ def retrieve_druids_for_instance_task(**kwargs):
 @task
 def trigger_digital_bookplate_979_task(**kwargs):
     instances = kwargs["instances"]
-    instances_list = [item for row in instances for item in row]
     dag_run_ids = []
-    logger.info(f"Total incoming lists of instances {len(instances)}")
-    total_instances: int = 0
-    for row in instances_list:
+    logger.info(f"Total incoming instances {len(instances)}")
+    for row in instances:
         if len(row) < 1:
             continue
-
-        total_instances += len(row.keys())
         for instance_uuid, funds in row.items():
             run_id = launch_digital_bookplate_979_dag(
                 instance_uuid=instance_uuid, funds=funds
             )
             dag_run_ids.append(run_id)
-
-    logger.info(f"Total incoming instances {total_instances}")
     return dag_run_ids
 
 
