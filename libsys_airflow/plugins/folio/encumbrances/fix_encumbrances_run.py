73286224a49aba1d9c00508e67ec2b3b9dd9165d
Joshua Greben <jgreben@stanford.edu>
2024-12-16 10:01:09 -0800

    Always return the log path for the fix_encumbrance_script

--- libsys_airflow/plugins/folio/encumbrances/fix_encumbrances_run.py
+++ libsys_airflow/plugins/folio/encumbrances/fix_encumbrances_run.py
@@ -1,7 +1,10 @@
 import asyncio
 import contextlib
+import logging
 import pathlib
 import libsys_airflow.plugins.folio.encumbrances.fix_encumbrances as fix_encumbrances_script
+
+logger = logging.getLogger(__name__)
 
 
 def fix_encumbrances_run(*args, **kwargs):
@@ -20,10 +23,14 @@ def fix_encumbrances_run(*args, **kwargs):
 
     with log_path.open("w+", 1) as log:
         with contextlib.redirect_stdout(log):
-            asyncio.run(
-                fix_encumbrances_script.run_operation(
-                    int(choice), fiscal_year_code, tenant, username, password
+            try:
+                asyncio.run(
+                    fix_encumbrances_script.run_operation(
+                        int(choice), fiscal_year_code, tenant, username, password
+                    )
                 )
-            )
+            except Exception as e:
+                logger.error(f"fix_encumbrance_script failed with: {e}")
+                return str(log_path.absolute())
 
     return str(log_path.absolute())
