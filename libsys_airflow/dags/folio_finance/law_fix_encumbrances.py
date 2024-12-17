cef8189147fb247759de06c55250d6527ad3d5d2
Joshua Greben <jgreben@stanford.edu>
2024-12-16 19:11:08 -0800

    Wait for upstream task to finish (do not use always trigger rule)

--- libsys_airflow/dags/folio_finance/law_fix_encumbrances.py
+++ libsys_airflow/dags/folio_finance/law_fix_encumbrances.py
@@ -63,7 +63,7 @@ with DAG(
     send_logs = PythonOperator(
         task_id="email_fix_encumbrances_log",
         python_callable=email_log,
-        trigger_rule='always',
+        trigger_rule='all_done',
         op_kwargs={
             "log_file": "{{ ti.xcom_pull('run_fix_encumbrances_script') }}",
             "fy_code": FY_CODE,
