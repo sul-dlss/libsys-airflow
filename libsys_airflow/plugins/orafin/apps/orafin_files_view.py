f7dc4d7228214618ef796377625507738a9d9076
Jeremy Nelson <jermnelson@gmail.com>
2024-12-16 17:07:12 -0800

    feat: Supports download of files

--- libsys_airflow/plugins/orafin/apps/orafin_files_view.py
+++ libsys_airflow/plugins/orafin/apps/orafin_files_view.py
@@ -1,6 +1,7 @@
 import datetime
 import pathlib
 
+from flask import send_file
 from flask_appbuilder import expose, BaseView as AppBuilderBaseView
 
 
@@ -17,13 +18,12 @@ def _file_info(file: pathlib.Path) -> dict:
 class OrafinFilesView(AppBuilderBaseView):
     default_view = "orafin_files_home"
     route_base = "/orafin"
-    files_base = "/opt/airflow/orafin-files"
+    files_base = pathlib.Path("/opt/airflow/orafin-files")
 
     @expose("/")
     def orafin_files_home(self):
-        orafin_base = pathlib.Path(self.files_base)
-        data = orafin_base / "data"
-        reports = orafin_base / "reports"
+        data = self.files_base / "data"
+        reports = self.files_base / "reports"
 
         feeder_files = []
         for feeder_file in data.iterdir():
@@ -37,4 +37,17 @@ class OrafinFilesView(AppBuilderBaseView):
 
         return self.render_template(
             "orafin/index.html", feeder_files=feeder_files, ap_reports=ap_reports
+        )
+
+    @expose("/<type_of>/<file_name>")
+    def download(self, type_of, file_name):
+        orafin_file_path = self.files_base / type_of / file_name
+        mimetype = "application/csv"
+        if type_of == "data":
+            mimetype = "application/text"
+        return send_file(
+            str(orafin_file_path),
+            as_attachment=True,
+            mimetype=mimetype,
+            download_name=file_name,
         )
