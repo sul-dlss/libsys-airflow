fcac4c222f8b27bae0171d2690a79f47c89ff4fd
Jeremy Nelson <jermnelson@gmail.com>
2025-02-12 15:03:58 -0800

    feat: Email and Archive helper functions

--- libsys_airflow/plugins/authority_control/email.py
+++ libsys_airflow/plugins/authority_control/email.py
@@ -1,7 +1,33 @@
 import logging
 
+from airflow.models import Variable
+
+from libsys_airflow.plugins.shared.utils import send_email_with_server_name
+
+from libsys_airflow.plugins.shared.utils import is_production, dag_run_url
+
 logger = logging.getLogger(__name__)
 
 
 def email_report(bash_result: str):
+    """
+    Emails result of folio-data-import to developers and SUL
+
+    Args:
+    - bash_result (str): result of the bash command
+    """
+
+    devs_email = Variable.get("EMAIL_DEVS")
+    to_emails = [devs_email]
+
+    if is_production:
+        sul_email = Variable.get("OCLC_EMAIL_SUL")
+        to_emails.append(sul_email)
+
+    body = f"""<p>{bash_result}</p><p>DAG Run<a href="{{ dag_run_url }}">{dag_run_url}</a>"""
+    send_email_with_server_name(
+        to=to_emails,
+        subject="Folio Data Import",
+        html_content=body,
+    )
     logger.info(f"Emailing load report: {bash_result}")
--- libsys_airflow/plugins/authority_control/helpers.py
+++ libsys_airflow/plugins/authority_control/helpers.py
@@ -1,11 +1,19 @@
 import logging
+import pathlib
 
 logger = logging.getLogger(__name__)
 
 
-def clean_up(marc_file_path):
+def clean_up(marc_file_path: str, airflow: str = '/opt/airflow'):
     """
     Moves marc file after running folio data import
     """
-    logger.info(f"Moving {marc_file_path} to archive")
+    marc_file_path = pathlib.Path(marc_file_path)
+    archive_dir = pathlib.Path(airflow) / "authorities/archive"
+    archive_dir.mkdir(parents=True, exist_ok=True)
+
+    archive_file = archive_dir / marc_file_path.name
+    marc_file_path.rename(archive_file)
+
+    logger.info(f"Moved {marc_file_path} to archive")
     return True
