cedd90936c6e155ce008d11aef1a3565f636e0f1
Joshua Greben <jgreben@stanford.edu>
2025-04-01 11:05:05 -0700

    Keep using FOLIO_EPOSH_DATE as Variable name

--- libsys_airflow/dags/data_exports/full_dump_retrieval.py
+++ libsys_airflow/dags/data_exports/full_dump_retrieval.py
@@ -63,7 +63,7 @@ with DAG(
             description="Remove excluded tags listed in marc/excluded_tags.pyfrom incoming record.",
         ),
         "from_date": Param(
-            Variable.get("EPOCH_DATE", "1885-11-11"),
+            Variable.get("FOLIO_EPOCH_DATE", "1885-11-11"),
             format="date",
             type="string",
             description="The earliest date to select record IDs from FOLIO.",
