932dc525f530c9cdc5640cbe34b1fb8adeaf9e8d
Joshua Greben <jgreben@stanford.edu>
2025-10-08 16:38:50 -0700

    Revert "ignore encoding errors when opening edi files to email"
    This reverts commit 131be158e16546cd313977775c6ac7bf7dd1c68e.

--- libsys_airflow/plugins/vendor/edi.py
+++ libsys_airflow/plugins/vendor/edi.py
@@ -3,6 +3,6 @@ import re
 
 
 def invoice_count(edit_path: pathlib.Path) -> int:
-    with edit_path.open("r", errors="ignore") as fo:
+    with edit_path.open("r") as fo:
         text = fo.read()
         return len(re.findall(r"BGM\+380", text))
