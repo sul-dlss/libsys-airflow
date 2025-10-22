e40c2935685b91e7041a925191fe4732ba1eaa47
Shelley Doljack <sdoljack@stanford.edu>
2025-10-22 17:40:55 -0600

    Updates import for pydantic deep_copy.

--- log_config.py
+++ log_config.py
@@ -1,5 +1,5 @@
 from copy import deepcopy
-from pydantic.utils import deep_update
+from pydantic.v1.utils import deep_update
 from airflow.config_templates.airflow_local_settings import DEFAULT_LOGGING_CONFIG
 
 LOGGING_CONFIG = deep_update(
