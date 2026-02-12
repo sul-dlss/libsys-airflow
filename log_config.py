2ddd399b7b5e8a19ff84e9f8f913a94cd9085c83
Jeremy Nelson <jermnelson@gmail.com>
2026-02-12 13:37:54 -0800

    feat: Upgrade to Airflow version 3.1.7

--- log_config.py
+++ log_config.py
@@ -2,6 +2,8 @@ from copy import deepcopy
 from pydantic.v1.utils import deep_update
 from airflow.config_templates.airflow_local_settings import DEFAULT_LOGGING_CONFIG
 
+REMOTE_TASK_LOG = None
+
 LOGGING_CONFIG = deep_update(
     deepcopy(DEFAULT_LOGGING_CONFIG),
     {
