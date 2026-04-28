c72fae79c8d7d6a5ed8b0529f44e94906ba15343
Shelley Doljack <sdoljack@stanford.edu>
2026-04-27 21:07:09 -0600

    Refactors create_connection_task to use API instead of DB Session.

--- tests/mocks.py
+++ tests/mocks.py
@@ -109,3 +109,14 @@ class MockLibraryConfig(pydantic.BaseModel):
     okapi_url: str = "https://okapi.edu"
     okapi_username: str = "admin"
     tenant_id: str = "a_lib"
+
+
+class MockAirflowApiClientConfig(pydantic.BaseModel):
+    host: str = "http://localhost:8080"
+    username: str = "user_name"
+    password: str = "napassword"
+    access_token: str = "YW4gYWNjZXNzIHRva2Vu"
+
+
+class MockAirflowApiClient(pydantic.BaseModel):
+    configuration: MockAirflowApiClientConfig
