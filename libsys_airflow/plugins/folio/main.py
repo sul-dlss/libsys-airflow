fbfc897804818c3606cdba647155c8eb204b5776
Shelley Doljack <sdoljack@stanford.edu>
2026-08-13 13:41:06 -0600

    Removes healthcheck from FOLIOPlugin menu.

--- libsys_airflow/plugins/folio/main.py
+++ libsys_airflow/plugins/folio/main.py
@@ -3,7 +3,6 @@ from airflow.plugins_manager import AirflowPlugin
 from libsys_airflow.plugins.folio.apps.circ_rules_tester_view import (
     app as circ_rules_tester_app,
 )
-from libsys_airflow.plugins.folio.apps.healthcheck_view import app as healthcheck_app
 
 circ_rules_tester_fastapi_app = {
     "app": circ_rules_tester_app,
@@ -17,20 +16,8 @@ circ_rules_tester_view = {
     "url_route": "circ_rule_tester",
 }
 
-healthcheck_fastapi_app = {
-    "app": healthcheck_app,
-    "url_prefix": "/healthcheck",
-    "name": "Healthcheck",
-}
-healthcheck_view = {
-    "name": "Healthcheck",
-    "category": "FOLIO",
-    "href": "/healthcheck/",
-    "url_route": "healthcheck",
-}
-
 
 class FOLIOPlugin(AirflowPlugin):
     name = "FOLIOInformation"
-    fastapi_apps = [circ_rules_tester_fastapi_app, healthcheck_fastapi_app]
-    external_views = [circ_rules_tester_view, healthcheck_view]
+    fastapi_apps = [circ_rules_tester_fastapi_app]
+    external_views = [circ_rules_tester_view]
